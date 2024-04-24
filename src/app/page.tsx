16b613cd965536e290f4322ee62265e09c82f2ac
jjhiggz <jonathan.higger@gmail.com>
2024-04-24 09:42:19 -0600

    next version good to go

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            By The Grace OF GAWWWWDD{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
